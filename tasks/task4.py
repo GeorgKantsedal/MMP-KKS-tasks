dd07d684a2ff9b345c44a0682d2aba8e8fbca1bf
Heorhii Kantsedal <georgorg261@gmail.com>
2023-04-01 09:12:19 +0300

    Added task4

---
+++ tasks/task4.py
@@ -0,0 +1,173 @@
+"""
+Instructions:
+
+Fill in the methods of the DataCleaner class to produce the same printed results
+as in the comments below. Good luck, and have fun!
+"""
+from __future__ import annotations
+import os
+
+import numpy as np
+import pandas as pd
+import pickle
+
+from typing import Dict, Any, List, Tuple
+
+
+class DataCleaner:
+    """
+    Transform a pandas df while keeping track of the history of transformations to
+    allow reverting back to earlier state.
+    """
+    def __init__(self, df: pd.DataFrame):
+        pass
+    
+    def __del__(self):
+        """
+        Dont forget to clean files on server after finish
+        """
+        pass
+
+
+
+transactions = pd.DataFrame(
+    {
+        "customer_id": [10, 10, 13, 10, 11, 11, 10],
+        "amount": [1.00, 1.31, 20.5, 0.5, 0.2, 0.2, np.nan],
+        "timestamp": [
+            "2020-10-08 11:32:01",
+            "2020-10-08 13:45:00",
+            "2020-10-07 05:10:30",
+            "2020-10-08 12:30:00",
+            "2020-10-07 01:29:33",
+            "2020-10-08 13:45:00",
+            "2020-10-09 02:05:21",
+        ]
+    }
+)
+transactions_dc = DataCleaner(transactions)
+
+print(f"Current dataframe:\n{transactions_dc.current}")
+
+# Current dataframe:
+#    customer_id  amount            timestamp
+# 0           10    1.00  2020-10-08 11:32:01
+# 1           10    1.31  2020-10-08 13:45:00
+# 2           13   20.50  2020-10-07 05:10:30
+# 3           10    0.50  2020-10-08 12:30:00
+# 4           11    0.20  2020-10-07 01:29:33
+# 5           11    0.20  2020-10-08 13:45:00
+# 6           10     NaN  2020-10-09 02:05:21
+
+print(f"Current dtypes:\n{transactions_dc.current.dtypes}")
+
+# Initial dtypes:
+# customer_id      int64
+# amount         float64
+# timestamp       object
+# dtype: object
+
+transactions_dc.adjust_dtype({"timestamp": np.datetime64})
+
+print(f"Changed dtypes to:\n{transactions_dc.current.dtypes}")
+
+# Changed dtypes to:
+# customer_id             int64
+# amount                float64
+# timestamp      datetime64[ns]
+
+transactions_dc.impute_missing(columns=["amount"])
+
+print(f"Imputed missing as overall mean:\n{transactions_dc.current}")
+
+# Imputed missing as mean:
+#    customer_id     amount           timestamp
+# 0           10   1.000000 2020-10-08 11:32:01
+# 1           10   1.310000 2020-10-08 13:45:00
+# 2           13  20.500000 2020-10-07 05:10:30
+# 3           10   0.500000 2020-10-08 12:30:00
+# 4           11   0.200000 2020-10-07 01:29:33
+# 5           11   0.200000 2020-10-08 13:45:00
+# 6           10   3.951667 2020-10-09 02:05:21
+
+print(f"History of changes:\n{transactions_dc.history}")
+
+# ** Any coherent structure with history of changes **
+# E.g., here's one possibility
+
+# History of changes:
+# [('Initial df',    customer_id  amount            timestamp
+# 0           10    1.00  2020-10-08 11:32:01
+# 1           10    1.31  2020-10-08 13:45:00
+# 2           13   20.50  2020-10-07 05:10:30
+# 3           10    0.50  2020-10-08 12:30:00
+# 4           11    0.20  2020-10-07 01:29:33
+# 5           11    0.20  2020-10-08 13:45:00
+# 6           10     NaN  2020-10-09 02:05:21), ("Adjusted dtypes using {'timestamp': <class 'numpy.datetime64'>}",    customer_id  amount           timestamp
+# 0           10    1.00 2020-10-08 11:32:01
+# 1           10    1.31 2020-10-08 13:45:00
+# 2           13   20.50 2020-10-07 05:10:30
+# 3           10    0.50 2020-10-08 12:30:00
+# 4           11    0.20 2020-10-07 01:29:33
+# 5           11    0.20 2020-10-08 13:45:00
+# 6           10     NaN 2020-10-09 02:05:21), ("Imputed missing in ['amount']",    customer_id     amount           timestamp
+# 0           10   1.000000 2020-10-08 11:32:01
+# 1           10   1.310000 2020-10-08 13:45:00
+# 2           13  20.500000 2020-10-07 05:10:30
+# 3           10   0.500000 2020-10-08 12:30:00
+# 4           11   0.200000 2020-10-07 01:29:33
+# 5           11   0.200000 2020-10-08 13:45:00
+# 6           10   3.951667 2020-10-09 02:05:21)]
+
+transactions_dc.save("transactions")
+loaded_dc = DataCleaner.load("transactions")
+print(f"Loaded DataCleaner current df:\n{loaded_dc.current}")
+
+# Loaded DataCleaner current df:
+#    customer_id     amount           timestamp
+# 0           10   1.000000 2020-10-08 11:32:01
+# 1           10   1.310000 2020-10-08 13:45:00
+# 2           13  20.500000 2020-10-07 05:10:30
+# 3           10   0.500000 2020-10-08 12:30:00
+# 4           11   0.200000 2020-10-07 01:29:33
+# 5           11   0.200000 2020-10-08 13:45:00
+# 6           10   3.951667 2020-10-09 02:05:21
+
+transactions_dc.revert()
+print(f"Reverting missing value imputation:\n{transactions_dc.current}")
+
+# Reverting missing value imputation:
+#    customer_id  amount           timestamp
+# 0           10    1.00 2020-10-08 11:32:01
+# 1           10    1.31 2020-10-08 13:45:00
+# 2           13   20.50 2020-10-07 05:10:30
+# 3           10    0.50 2020-10-08 12:30:00
+# 4           11    0.20 2020-10-07 01:29:33
+# 5           11    0.20 2020-10-08 13:45:00
+# 6           10     NaN 2020-10-09 02:05:21
+
+
+def solution(transactions):
+    rez = []
+    
+    transactions_dc = DataCleaner(transactions)
+    rez.append(f"Current dataframe:\n{transactions_dc.current}")
+    rez.append(f"Current dtypes:\n{transactions_dc.current.dtypes}")
+    transactions_dc.adjust_dtype({"timestamp": np.datetime64})
+    rez.append(f"Changed dtypes to:\n{transactions_dc.current.dtypes}")
+
+    transactions_dc.impute_missing(columns=["amount"])
+    rez.append(f"Changed dtypes to:\n{transactions_dc.current.dtypes}")
+    rez.append(f"Imputed missing as overall mean:\n{transactions_dc.current}")
+    rez.append(f"History of changes:\n{transactions_dc.history}")
+
+    transactions_dc.save("transactions")
+    loaded_dc = DataCleaner.load("transactions")
+    rez.append(f"Loaded DataCleaner current df:\n{loaded_dc.current}")
+
+    transactions_dc.revert()
+    rez.append(f"Reverting missing value imputation:\n{transactions_dc.current}")
+    return rez
+
+if __name__ == "__main__":
+    pass
