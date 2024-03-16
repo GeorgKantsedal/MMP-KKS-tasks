ed10d498a3c4861a689c5cd75aaef29d70beba79
Heorhii Kantsedal <georgorg261@gmail.com>
2024-03-16 11:01:06 +0200

    Added task 6

---
+++ tasks/task6.py
@@ -0,0 +1,8 @@
+def solution():
+    """
+    Return features that correlate with target.
+    Returns:
+        list - list of feature names that correlate with target
+    """
+
+    return ["feature1", "feature2"]
